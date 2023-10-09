55708fd5483a074afbabafd858b9246d4cb4544d
pratheek <pratheekmanimaran@gmail.com>
2023-10-09 20:19:37 +0530

    Modified some files

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('delete/<list_id>',views.delete,name='delete'),
-    path('cross_off/<list_id>',views.cross_off,name='cross_off'),
-    path('uncross/<list_id>',views.uncross,name='uncross'),
-    path('edit/<list_id>',views.edit,name='edit'),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('delete/<list_id>',delete, name='delete'),
+    path('cross_off/<list_id>',cross_off, name='cross_off'),
+    path('uncross/<list_id>',uncross, name='uncross'),
+    path('edit/<list_id>',edit, name='edit'),
 ]
